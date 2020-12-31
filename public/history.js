59e1041902693c8f142917eb1441bfe2e88b6740
Austindrum <drum108306@gmail.com>
2020-12-31 23:07:30 +0800

    fixed: css grid

--- public/history.js
+++ public/history.js
@@ -1,4 +1,8 @@
-var prices = Array.from(document.querySelectorAll(".meal-price"));
+var tempprices = document.querySelectorAll(".meal-price");
+var prices = [];
+tempprices.forEach(function(price){
+    prices.push(price);
+})
 var totalPrice = document.getElementById("price");
 var priceAdd = 0;
 for (let i = 0; i < prices.length; i++) {
